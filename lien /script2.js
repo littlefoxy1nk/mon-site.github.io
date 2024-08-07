ba85311105daf9047e5e2973e91f069ef8c2306e
Hamoir, Chloé <chloehamoir@gmail.com>
2024-08-07 10:05:20 +0200

    JS2

--- lien /script2.js
+++ lien /script2.js
@@ -2,16 +2,16 @@ let dark = false;
 
 function darkMode() {
     let background = document.body;
-    let backbox = document.querySelectorAll('expl');
+    let backbox = document.querySelectorAll('boite');
 
     if (!dark) {
         background.style.backgroundImage = url('images/beige.jpeg');
-        backbox.forEach(function(expl) {
+        backbox.forEach(function(boite) {
             p.style.color = "#faeee0";
         });
     } else {
         background.style.backgroundColor = ""; // reset to default
-        backbox.forEach(function(expl) {
+        backbox.forEach(function(boite) {
             p.style.color = ""; // reset to default
         });
     }
