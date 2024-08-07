1d311aa5df9ed08ee937f37165f95928c24d4a80
Hamoir, Chloé <chloehamoir@gmail.com>
2024-08-07 13:29:17 +0200

    JS message d'entrée

--- lien /script4.js
+++ lien /script4.js
@@ -4,25 +4,18 @@
 function darkMode() {
     let background = document.body;
     let paragraphs = document.querySelectorAll('p');
-    let tirets = document.querySelectorAll('.interet,.langues');
-    console.log(tirets)
 
     if (!dark) {
         background.style.backgroundColor = "#545454";
         paragraphs.forEach(function(p) {
             p.style.color = "#faeee0";
         });
-        tirets.forEach(function(t) {
-            t.style.color="#545454"
-        })
+
     } else {
         background.style.backgroundColor = ""; 
         paragraphs.forEach(function(p) {
             p.style.color = ""; 
         });
-        tirets.forEach(function(t) {
-            t.style.color="#545454"
-        })
     }
 
     dark = !dark;
