7945485ac6b499ebcd1f8e557a0e8f5f845140ed
Hamoir, Chloé <chloehamoir@gmail.com>
2024-08-07 11:58:10 +0200

    Js4

--- lien /script4.js
+++ lien /script4.js
@@ -4,20 +4,24 @@
 function darkMode() {
     let background = document.body;
     let paragraphs = document.querySelectorAll('p');
-    let tirets = document.getElementById ('.interet');
+    let tirets = document.qweryselector('.interet');
 
     if (!dark) {
         background.style.backgroundColor = "#545454";
         paragraphs.forEach(function(p) {
             p.style.color = "#faeee0";
         });
-        tirets.style.color='#faeee0';
+        tirets.forEach(function(tirets) {
+            tirets.style.color="#545454"
+        })
     } else {
         background.style.backgroundColor = ""; 
         paragraphs.forEach(function(p) {
             p.style.color = ""; 
         });
-        tirets.style.color="";
+        tirets.forEach(function(tirets) {
+            tirets.style.color="#545454"
+        })
     }
 
     dark = !dark;
